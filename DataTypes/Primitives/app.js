9fc4dd20b1a3ed1ef00c3f19f6954abb706469c5
sourabhchalke <sourabhchalke2157@gmail.com>
2025-06-30 18:59:57 +0530

    Updates primitive data-type

--- DataTypes/Primitives/app.js
+++ DataTypes/Primitives/app.js
@@ -1,2 +1,16 @@
-var s = "Sourabh";
-
+"use strict";
+// Primitive Types : These are immutable and represent single values.These are pass-by-value and stored in stack memory
+// Number
+let num = 5;
+// String
+let names = "Virat";
+// Boolean
+let bool = true;
+// Null
+let n = null;
+// Undefined
+let undef;
+// Symbol
+let sym = Symbol("id");
+// BigInt
+let bint;
--- DataTypes/Primitives/app.ts
+++ DataTypes/Primitives/app.ts
@@ -1,8 +1,16 @@
-// Primitive Types
+// Primitive Types : These are immutable and represent single values.These are pass-by-value and stored in stack memory
 
-let num=5;
-// num="N" not in typescript 
-let names="Virat";
-let bool=true;
-let kkk: any;
-let u:unknown;
+// Number
+let num:number=5;
+// String
+let names:string="Virat";
+// Boolean
+let bool:boolean=true;
+// Null
+let n:null=null;
+// Undefined
+let undef:undefined;
+// Symbol
+let sym=Symbol("id");
+// BigInt
+let bint:bigint;
