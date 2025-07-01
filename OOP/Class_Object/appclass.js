d22bb215b42a6f09126cfc2d01b9c2de90cf250b
sourabhchalke <sourabhchalke2157@gmail.com>
2025-07-01 19:44:05 +0530

    Constructor created

--- OOP/Class_Object/appclass.js
+++ OOP/Class_Object/appclass.js
@@ -10,3 +10,21 @@ class Employee {
 }
 let obj1 = new Employee();
 let obj2 = new Employee();
+// Constructor
+class User {
+    constructor(name, age, address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+}
+let u1 = new User("Rohit", 30, "Pune");
+let u2 = new User("Vijay", 25, "Mumbai");
+class User2 {
+    constructor(name, age, role) {
+        this.name = name;
+        this.age = age;
+        this.role = role;
+    }
+}
+let user1 = new User2("Rakesh", 26, "HR");
--- OOP/Class_Object/appclass.ts
+++ OOP/Class_Object/appclass.ts
@@ -8,3 +8,22 @@ class Employee{
 
 let obj1 = new Employee();
 let obj2 =new Employee();
+
+// Constructor
+class User{
+    constructor(public name:string,public age:number,public address:string){}
+}
+let u1 = new User("Rohit",30,"Pune");
+let u2 = new User("Vijay",25,"Mumbai");
+
+class User2 {
+public name:string;
+public age:number;
+public role:string;
+    constructor(name:string,age:number,role:string){
+        this.name=name;
+        this.age=age;
+        this.role=role;
+    }
+}
+let user1 = new User2("Rakesh",26,"HR");
