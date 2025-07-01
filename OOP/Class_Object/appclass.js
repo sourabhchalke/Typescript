769932b8a65170baccb00b979e341b06c15cc9be
sourabhchalke <sourabhchalke2157@gmail.com>
2025-07-01 18:58:46 +0530

    Simple Class Example

---
+++ OOP/Class_Object/appclass.js
@@ -0,0 +1,12 @@
+"use strict";
+// Class
+class Employee {
+    constructor() {
+        this.name = "Rakesh";
+        this.age = 23;
+        this.address = "Pune";
+        this.role = "HR";
+    }
+}
+let obj1 = new Employee();
+let obj2 = new Employee();
---
+++ OOP/Class_Object/appclass.ts
@@ -0,0 +1,10 @@
+// Class
+class Employee{
+    name="Rakesh";
+    age=23;
+    address="Pune";
+    role="HR"
+}
+
+let obj1 = new Employee();
+let obj2 =new Employee();
