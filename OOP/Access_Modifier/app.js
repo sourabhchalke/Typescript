ac4b3936418044c2f39f720609c799a8a49ee01e
sourabhchalke <sourabhchalke2157@gmail.com>
2025-07-02 21:42:59 +0530

    Private access modifier added

--- OOP/Access_Modifier/app.js
+++ OOP/Access_Modifier/app.js
@@ -10,3 +10,24 @@ class User1 {
 let obj = new User1();
 console.log(obj.name); //Accessing outside the class
 obj.name = "Kohli";
+// Private = Accessible only within the class where it is defined.Cannot be accessed from outside the class or by subclasses.
+class Employee1 {
+    constructor() {
+        this.name = "Rohit Sharma";
+    }
+    getName() {
+        console.log("Private Variable :", this.name);
+    }
+    getAddress() {
+        console.log("Private Method");
+    }
+}
+class Employee2 extends Employee1 {
+    getNameP() {
+        console.log(this.name); //name is not accessible here,because it is private
+    }
+}
+let obj4 = new Employee1();
+// obj4.name="Dhoni"; Cannot access outside class
+obj4.getName();
+// obj4.getAddress(); Only access within class
