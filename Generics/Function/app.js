2b91e5b994cddcc8c2f0a38816ce3b25627cae30
sourabhchalke <sourabhchalke2157@gmail.com>
2025-07-06 21:03:38 +0530

    Generic function example created

---
+++ Generics/Function/app.js
@@ -0,0 +1,6 @@
+"use strict";
+// Generic Function Example
+function multiple(a) {
+    return a[3];
+}
+console.log(multiple([1, 2, 34, 4, 5]));
---
+++ Generics/Function/app.ts
@@ -0,0 +1,5 @@
+// Generic Function Example
+function multiple<T>(a:T[]):T{
+    return a[3];
+}
+console.log(multiple([1,2,34,4,5]));
