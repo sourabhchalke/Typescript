ef04674ac7e45a41a61a321007e13169bfdf677a
sourabhchalke <sourabhchalke2157@gmail.com>
2025-07-19 06:52:23 -0700

    Multiple Type Parameter added

--- Generics/Function/app.js
+++ Generics/Function/app.js
@@ -4,3 +4,10 @@ function multiple(a) {
     return a[3];
 }
 console.log(multiple([1, 2, 34, 4, 5]));
+// Multiple Type Parameter
+function merge(obj1, obj2) {
+    return Object.assign(Object.assign({}, obj1), obj2);
+}
+const result = merge({ name: "Sourabh" }, { age: 25 });
+console.log(result.name); // Sourabh
+console.log(result.age); // 25
--- Generics/Function/app.ts
+++ Generics/Function/app.ts
@@ -3,3 +3,12 @@ function multiple<T>(a:T[]):T{
     return a[3];
 }
 console.log(multiple([1,2,34,4,5]));
+
+// Multiple Type Parameter
+function merge<T, U>(obj1: T, obj2: U): T & U {
+    return { ...obj1, ...obj2 };
+  }
+  
+  const result = merge({ name: "Sourabh" }, { age: 25 });
+  console.log(result.name); // Sourabh
+  console.log(result.age);  // 25
