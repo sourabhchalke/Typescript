085e95c166223311f439e751194d4bd8e82265fa
sourabhchalke <sourabhchalke2157@gmail.com>
2025-07-22 18:40:48 +0530

    Index signature and Type assertion examples

--- Practice/Interface/app.js
+++ Practice/Interface/app.js
@@ -25,3 +25,30 @@ const emp2 = {
 // emp2.name="Darshan";  Error: Cannot assign to 'name' because it is a read-only property
 getEmployeeInfo(emp1);
 getEmployeeInfo(emp2);
+const obj = {
+    Ajay: 8475874857,
+    Rohit: 9876543234,
+    Vaibhav: 9878676545,
+    Rohan: 7798563498
+};
+console.log("Ajay :", obj["Ajay"]);
+console.log("Rohan :", obj);
+const user1 = {
+    name: "Nikhil",
+    age: 27,
+    student: false
+};
+console.log(user1);
+// Type Assertions
+// Angle Bracket syntax
+let pm = "Narendra";
+let pmLength = pm.length;
+console.log("Length of pm :", pmLength);
+// as Syntax(Recommended for JSX/React)
+let cm = "Devendra Fadnavis";
+let cmLength = cm.length;
+console.log("Length of cm :", cmLength);
+// Ex
+let data = "42";
+let num = data;
+console.log(Number(num) + 10); //Convert "42" string into 42 as Number and then add 10 into it.
--- Practice/Interface/app.ts
+++ Practice/Interface/app.ts
@@ -41,3 +41,39 @@ const emp2={
 
 getEmployeeInfo(emp1);
 getEmployeeInfo(emp2);
+
+// Index Signature
+interface Customer{
+    [name:string]:number;
+}
+const obj:Customer={
+    Ajay:8475874857,
+    Rohit:9876543234,
+    Vaibhav:9878676545,
+    Rohan:7798563498
+}
+console.log("Ajay :",obj["Ajay"]);
+console.log("Rohan :",obj);
+// Real-World Use Case Index Signature:Dynamic Form Data
+interface formData{
+    [field:string]:string | number | boolean;
+}
+const user1:formData={
+    name:"Nikhil",
+    age:27,
+    student:false
+}
+console.log(user1);
+// Type Assertions
+// Angle Bracket syntax
+let pm:string="Narendra";
+let pmLength=(<string>pm).length;
+console.log("Length of pm :",pmLength);
+// as Syntax(Recommended for JSX/React)
+let cm:string="Devendra Fadnavis";
+let cmLength=(cm as string).length;
+console.log("Length of cm :",cmLength);
+// Ex
+let data:any="42";
+let num=(data as string);
+console.log(Number(num)+10);//Convert "42" string into 42 as Number and then add 10 into it.
