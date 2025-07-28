87501999aec82da85ab3eac8fe13798373dd3e72
sourabhchalke <sourabhchalke2157@gmail.com>
2025-07-28 12:25:09 +0530

    Itentity function and Merge function generic which contain only 'object'

--- Practice/Constraint/app.js
+++ Practice/Constraint/app.js
@@ -35,7 +35,33 @@ function makeAnimal(obj) {
 }
 const animal1 = new Animals();
 makeAnimal(animal1);
-// makeAnimal({});  Error: {} does not extend Animal
-// This is for when T extends Dogs class
-// const dog1=new Dogs();
+// makeAnimal({});  Error: {} does not extend Animal;
+// const dog1=new Dogs(); // This is for when T extends Dogs class
 // makeAnimal(dog1); 
+// 4.Write a generic function identity<T>(arg: T): T but constrain T so it must be an object
+function identity(obj) {
+    console.log(obj);
+}
+const identity1 = {
+    name: "Virat",
+    email: "virat18@gmail.com",
+    password: 189818
+};
+identity(identity1);
+// identity("String");Error-It contain only objects
+// 5.Create a generic function merge<T, U>(obj1: T, obj2: U) and return a combined object. Constrain T and U to be objects only.
+function merge(obj1, obj2) {
+    return Object.assign(Object.assign({}, obj1), obj2);
+}
+const empInfo1 = {
+    id: 1,
+    name: "Gaurav Pawar",
+    address: "Akurdi"
+};
+const empSalary1 = {
+    salary: 25000,
+    pf: 2500,
+    incentive: 3000
+};
+console.log(merge(empInfo1, empSalary1));
+// console.log(merge("dhdhdhd"));Error : It contain only object
--- Practice/Constraint/app.ts
+++ Practice/Constraint/app.ts
@@ -34,16 +34,57 @@ class Dogs extends Animals{
         console.log("Dog Barking");
     }
 }
-
 function makeAnimal<T extends Animals>(obj:T):void{
     obj.move();
     // obj.barking(); Error T extends Animal(parent class)
-    
 }
 const animal1=new Animals();
 makeAnimal(animal1);
-// makeAnimal({});  Error: {} does not extend Animal
-
-// This is for when T extends Dogs class
-// const dog1=new Dogs();
+// makeAnimal({});  Error: {} does not extend Animal;
+// const dog1=new Dogs(); // This is for when T extends Dogs class
 // makeAnimal(dog1); 
+
+// 4.Write a generic function identity<T>(arg: T): T but constrain T so it must be an object
+function identity<T extends object>(obj:T):void{
+    console.log(obj);
+}
+type Identity={
+    name:string,
+    email:string,
+    password:number
+}
+const identity1:Identity={
+    name:"Virat",
+    email:"virat18@gmail.com",
+    password:189818
+}
+identity(identity1);
+// identity("String");Error-It contain only objects
+
+// 5.Create a generic function merge<T, U>(obj1: T, obj2: U) and return a combined object. Constrain T and U to be objects only.
+function merge<T,U extends object>(obj1:T,obj2:U):T&U{
+    return {...obj1,...obj2}
+}
+type empInfo={
+    id:number,
+    name:string,
+    address:string
+}
+const empInfo1:empInfo={
+    id:1,
+    name:"Gaurav Pawar",
+    address:"Akurdi"
+}
+type empSalary={
+    salary:number,
+    pf:number,
+    incentive:number
+}
+const empSalary1:empSalary={
+    salary:25000,
+    pf:2500,
+    incentive:3000
+}
+console.log(merge(empInfo1,empSalary1));
+// console.log(merge("dhdhdhd"));Error : It contain only object
+
