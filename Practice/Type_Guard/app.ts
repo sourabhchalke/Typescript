9da125b0b0a130823da6117534e29aa1622fd002
sourabhchalke <sourabhchalke2157@gmail.com>
2025-07-29 15:17:20 +0530

    Basic example of typeOf and instanceof created

---
+++ Practice/Type_Guard/app.ts
@@ -0,0 +1,56 @@
+// TypeScript may not always know the exact type of a variable, especially when using union types. Type guards help narrow down these types at runtime.
+// Guard Type  	             Syntax                	    Use For
+// 1.typeof	               typeof x === "..."	      Primitives
+// 2.instanceof	           x instanceof Type	      Class instances
+// 3.in	                  "prop" in x	              Checking object properties
+// 4.Custom Guard          x is SomeType	          Advanced, reusable checks
+
+// Examples
+// 1.Type Guard with typeof
+// Create a function logValue that accepts a parameter of type string | number and logs whether it's a string or a number using typeof.
+function logValue(value:string | number){
+    if(typeof value ==="string"){
+        console.log("Your value is String:",value);
+    }else if(typeof value === "number"){
+        console.log("Your value is Number :",value);
+    }else{
+        console.log("Your value is not either string or number:",value);
+    }
+}
+logValue("Welcome to Type Guard Concept.");
+logValue(34);
+
+// 2.Type Guard with instanceof
+// Define two classes Car and Bike. Create a function getVehicleType that takes an object and returns its type using instanceof.
+class CarT{
+    car():void{
+        console.log("Car...");
+    }
+}
+class BikeT{
+    bike():void{
+        console.log("Bike...");
+    }
+}
+function getVehicleType(obj: CarT | BikeT){
+    if(obj instanceof CarT){
+        obj.car();
+    }else if(obj instanceof BikeT){
+        obj.bike();
+    }
+}
+
+getVehicleType(new CarT);
+getVehicleType(new BikeT);
+
+// 3.Simple Union Type Guard
+// Write a function printId(id: number | string) that behaves differently based on whether id is a number or a string.
+function printId(id:number|string){
+    if(typeof id==="number"){
+        console.log(`Square of number ${id} : ${id*id}`,);
+    }else if(typeof id==="string"){
+        console.log(`Converting string to uppercase : ${id.toLocaleUpperCase()}`);
+    }
+}
+printId(5);
+printId("skfhjhdf");
